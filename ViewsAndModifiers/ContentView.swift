afaea63e60d558e63fb70bc2502401236974e6a6
Swifty1235 <Pedro.romero102008@gmail.com>
2024-12-31 17:16:17 -0500

    Day 23

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,24 +7,45 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CapsuleText: View {
+    var text: String
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello my name is Pedro!")
-                .font(.largeTitle)
-                
-            
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background (.indigo)
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
     }
 }
 
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundStyle(.white)
+            CapsuleText(text: "Second")
+                .foregroundStyle(.yellow)
+        }
+    }
+}
 
 #Preview {
     ContentView()
 }
 
+
+
+/*
+Image(systemName: "globe")
+    .imageScale(.large)
+    .foregroundStyle(.tint)
+Text("Hello my name is Pedro!")
+    .font(.largeTitle)
+
+let age = 17
+let canVote = age >= 18 ? "Yes" : "No"
+Text(canVote)
+
+*/
